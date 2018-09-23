7fcc163b0ef144c235480659c83d5191f7794f77
Vova Soroka <vsoroka@oroinc.com>
2018-09-23 20:20:48 +0300

    BAP-17535: Decrease the number of EXISTS calls to Redis (#20026)

--- OroRedisConfigBundle.php
+++ OroRedisConfigBundle.php
@@ -2,8 +2,22 @@
 
 namespace Oro\Bundle\RedisConfigBundle;
 
+use Oro\Bundle\RedisConfigBundle\DependencyInjection\Compiler\DoctrineCacheCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * The RedisConfigBundle bundle class.
+ */
 class OroRedisConfigBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DoctrineCacheCompilerPass());
+    }
 }
