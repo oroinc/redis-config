a630e49ac7203387abc33156a522970e39ce20a4
Dmitry Khrysev <x86demon@users.noreply.github.com>
2019-01-31 17:02:49 +0200

    BB-15098: Use key-value cache for slugs when redis is configured implementation (#22156)
    - enable key_value for slugs when redis is configured and enabled
    - check redis cache enabled before switching oro_redirect.url_cache_type to key_value
    - improved DirectUrlProcessor exception handling
    - fixed mset with empty changeset - fixed rollback handling

---
+++ DependencyInjection/RedisEnabledCheckTrait.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Oro\Bundle\RedisConfigBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * This trait contains methods to validate and check that redis is eenabled
+ * and configured as a cache
+ *
+ * It works with ContainerBuilder and can be used in compiler passes
+ */
+trait RedisEnabledCheckTrait
+{
+    /**
+     * @param ContainerBuilder $container
+     * @param string $paramName
+     * @return bool
+     */
+    private function validateRedisConfigDsnValue(ContainerBuilder $container, $paramName)
+    {
+        if (!$container->hasParameter($paramName) || (null === $container->getParameter($paramName))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return bool
+     */
+    protected function isRedisEnabledForSessions(ContainerBuilder $container)
+    {
+        if ($this->validateRedisConfigDsnValue($container, 'redis_dsn_session')
+            && self::REDIS_SESSION_HANDLER == $container->getParameter('session_handler')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return bool
+     */
+    protected function isRedisEnabledForCache(ContainerBuilder $container)
+    {
+        if ($this->validateRedisConfigDsnValue($container, 'redis_dsn_cache')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return bool
+     */
+    protected function isRedisEnabledForDoctrine(ContainerBuilder $container)
+    {
+        if ($this->validateRedisConfigDsnValue($container, 'redis_dsn_doctrine')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return bool
+     */
+    public function isRedisEnabled(ContainerBuilder $container)
+    {
+        return $this->isRedisEnabledForSessions($container)
+            || $this->isRedisEnabledForCache($container)
+            || $this->isRedisEnabledForDoctrine($container);
+    }
+}
--- Tests/Unit/DependencyInjection/Compiler/ConfigCompilerPassTest.php
+++ Tests/Unit/DependencyInjection/Compiler/ConfigCompilerPassTest.php
@@ -2,25 +2,75 @@
 
 namespace Oro\Bundle\RedisConfigBundle\Tests\Unit\DependencyInjection\Compiler;
 
+use Oro\Bundle\RedisConfigBundle\Configuration\Options;
 use Oro\Bundle\RedisConfigBundle\DependencyInjection\Compiler\ConfigCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 
 class ConfigCompilerPassTest extends \PHPUnit\Framework\TestCase
 {
-    public function testProcess()
+    /**
+     * @dataProvider definitionDataProvider
+     *
+     * @param Definition $extension
+     */
+    public function testProcess(Definition $extension)
     {
         $argumentValue = '~';
         $container = new ContainerBuilder();
-        $extension = new Definition();
-        $configCompilerPass = new ConfigCompilerPass();
-        $extension->setClass('Oro\Bundle\RedisConfigBundle\Configuration\Options');
+
+        $extension->setClass(Options::class);
+
         $container->setDefinition('oro.redis_config.configuration_option', $extension);
         $container->setParameter('redis_sentinel_prefer_slave', $argumentValue);
+
+        $configCompilerPass = new ConfigCompilerPass();
         $configCompilerPass->process($container);
+
         $this->assertEquals(
             $argumentValue,
-            $container->getDefinition('oro.redis_config.configuration_option')->getArgument(0)
+            $container->getDefinition('oro.redis_config.configuration_option')->getArgument(1)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function definitionDataProvider()
+    {
+        return [
+            'no args' => [(new Definition())],
+            '1 arg' => [(new Definition())->addArgument([])],
+            'more than 1 arg' => [(new Definition())->addArgument([])->addArgument('127.0.0.1')],
+        ];
+    }
+
+    public function testConfigSlugCacheWithoutEnabledRedisCache()
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter(ConfigCompilerPass::URL_CACHE_TYPE, ConfigCompilerPass::URL_CACHE_STORAGE);
+
+        $compilerPass = new ConfigCompilerPass();
+        $compilerPass->process($container);
+
+        $this->assertEquals(
+            ConfigCompilerPass::URL_CACHE_STORAGE,
+            $container->getParameter(ConfigCompilerPass::URL_CACHE_TYPE)
+        );
+    }
+
+    public function testConfigSlugCacheWithEnabledRedisCache()
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter(ConfigCompilerPass::URL_CACHE_TYPE, ConfigCompilerPass::URL_CACHE_STORAGE);
+        $container->setParameter('redis_dsn_cache', 'redis://127.0.0.1:6379/0');
+
+        $compilerPass = new ConfigCompilerPass();
+        $compilerPass->process($container);
+
+        $this->assertEquals(
+            ConfigCompilerPass::URL_CACHE_KEY_VALUE,
+            $container->getParameter(ConfigCompilerPass::URL_CACHE_TYPE)
         );
     }
 }
